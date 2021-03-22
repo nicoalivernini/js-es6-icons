ed9f8bb3928d160b7cab5161664eff5704552eee
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-22 15:12:27 +0100

    Inizio j-snack 1

---
+++ Jsnack/Jsnack-1/main.js
@@ -0,0 +1,29 @@
+// Creare un array di oggetti: ogni oggetto
+// descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+// Usiamo il forEach
+
+const bici = [
+  {
+    nome : 'Sasso',
+    peso : 13
+  },
+  {
+    nome : 'Aria',
+    peso : 3
+  },
+  {
+    nome : 'Marmo',
+    peso : 20
+  },
+  {
+    nome : 'Sabbia',
+    peso : 5
+  },
+  {
+    nome : 'Asfalto',
+    peso : 8
+  }
+];
+
+console.log(bici);
